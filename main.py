2717f85c0af916caa47294313bd4687a775f74ff
Greg Hawthorne <ghawthorne@me.com>
2020-11-21 23:53:49 +0100

    complete car manager and user

--- main.py
+++ main.py
@@ -6,9 +6,41 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor("lightgray")
+screen.title("Frogger")
 screen.tracer(0)
+
+car_manager = CarManager()
+player = Player()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+screen.onkey(player.go_down, "Down")
+screen.onkey(player.go_left, "Left")
+screen.onkey(player.go_right, "Right")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect a successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+
+
+
+screen.exitonclick()
